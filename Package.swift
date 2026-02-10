a5adfe3e0310733ec38390ee4ecd439f8eeee66e
michaelleechoicetech <m.lee@choicetech.kr>
2026-02-10 15:09:24 +0900

    외부 라이브러리 검증 11

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "CTKCNDP",
             url: "https://static.uwb.app/ctk/cndpsdk/CTKCNDP.xcframework.zip",
-            checksum : "af62ed9d77cc4870c568cadbffbf47801de38124ae28261df42e526d28b62524"
+            checksum : "13f2ca072a882a9f091aa581c11f56399a597ecbad80a3f91080db116179b6d0"
         )
     ]
 )
