72d55b1eb6cecad88ebf654fc442f219f709a3d9
michaelleechoicetech <m.lee@choicetech.kr>
2026-02-10 20:52:14 +0900

    외부 라이브러리 검증 22

--- Package.swift
+++ Package.swift
@@ -8,16 +8,26 @@ let package = Package(
         .macOS(.v12)
     ],
     products: [
+        // iOS Only (Device Only, Smaller Size)
         .library(
             name: "CTKCNDP",
             targets: ["CTKCNDP"]),
+        // Universal (Device + Simulator + macOS)
+        .library(
+            name: "CTKCNDP_Universal",
+            targets: ["CTKCNDP_Universal"])
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
             name: "CTKCNDP",
             url: "https://static.uwb.app/ctk/cndpsdk/CTKCNDP.xcframework.zip",
-            checksum : "ca90d30c0b9589fc32093a3d0c024d52ff60c015aa95a504ee51a57dc4cef48a"
+            checksum: "c139688b7cd44b3a68a12d35f5a42f46fcbff550d41af205ba8540c5dcc01f4e"
+        ),
+        .binaryTarget(
+            name: "CTKCNDP_Universal",
+            url: "https://static.uwb.app/ctk/cndpsdk/CTKCNDP_Universal.xcframework.zip",
+            checksum: "f69e5e36938c44dab9e01f75f60c6c133140e8a0112742e622fd5b438e1adae9"
         )
     ]
 )
