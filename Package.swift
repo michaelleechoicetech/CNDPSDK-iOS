2bd0515cdea70c0b98e58555e3cc4b06873e1b2f
michaelleechoicetech <m.lee@choicetech.kr>
2026-02-10 12:31:18 +0900

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "CTKCNDP_Distribution",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "CTKCNDP",
+            targets: ["CTKCNDP"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "CTKCNDP",
+            url: "https://static.uwb.app/ctk/cndpsdk/CTKCNDP.xcframework.zip",
+            checksum : "580dbf9d767d44384c3dc37b31143f2aa13c79866d79ed1f239a94bdcc85249c"
+        )
+    ]
+)
